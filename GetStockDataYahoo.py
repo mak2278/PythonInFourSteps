61d580d5a1c9a508f0bca868e3f2ecdb8a29d513
Marko Paris <Marko.paris@gmail.com>
2019-03-02 13:49:38 -0800

    Update GetStockDataYahoo.py

--- GetStockDataYahoo.py
+++ GetStockDataYahoo.py
@@ -1,25 +1,15 @@
-import fix_yahoo_finance as fyf
-
-import pandas as pd
-
-import pandas_datareader as pdr
-
+import pandas as pd 
+import pandas_datareader as dr 
 import matplotlib.pyplot as plt
 
-fyf.pdr.override()
-
-df= pdr.data.get_data_yahoo('aapl', start='2018-01-01', end='2019-01-25')
-
+df= dr.data.get_data_yahoo('aapl', start='2018-01-01', end='2019-01-25')
 df.loc['2017-01-01':'2019-01-25',['Open', 'Close']].plot(figsize=(16,9))
+#df['Close'].pct_change().plot(figsize=(16,9))
 
 plt.xlabel('Date')
-
 plt.ylabel('Price')
-
-plt.title('AAPL\n Source:Yahoo Finance')
-
+plt.title('AAPL Open & Close \n Data Source:Yahoo Finance')
 plt.legend()
 
 plt.grid(True)
-
 plt.show()
